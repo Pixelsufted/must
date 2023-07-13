908d31fa8ff8a3643f91757cffdb88611376fe54
Pixelsuft <yuskivl07@mail.ru>
2023-07-13 13:32:45 +0700

    fix log

--- log.py
+++ log.py
@@ -2,8 +2,8 @@ import sys
 
 
 def warn(*args) -> None:
-    sys.stderr.write('[WARN]: ' + ' '.join([str(_x) for _x in args]))
+    sys.stderr.write('[WARN]: ' + ' '.join([str(_x) for _x in args]) + '\n')
 
 
 def info(*args) -> None:
-    sys.stdout.write('[INFO]: ' + ' '.join([str(_x) for _x in args]))
+    sys.stdout.write('[INFO]: ' + ' '.join([str(_x) for _x in args]) + '\n')
