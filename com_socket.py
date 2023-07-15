40c12997090c6ec361b1566643fe8e0516f7eb60
Pixelsuft <yuskivl07@mail.ru>
2023-07-15 12:45:57 +0700

    upd

--- com_socket.py
+++ com_socket.py
@@ -1,6 +1,6 @@
-import asyncio
 import socket
 import threading
+import log
 import com_base
 
 
@@ -24,7 +24,10 @@ class SocketServer(com_base.BaseServer):
     def accept_clients(self) -> None:
         while self.running:
             conn, addr = self.sock.accept()
-            print(conn, addr)
+            threading.Thread(target=self.client_thread, args=(conn, addr)).start()
+
+    def client_thread(self, conn: socket.socket, addr: tuple) -> None:
+        print(conn, addr)
 
     def destroy(self) -> None:
         self.running = False
@@ -35,5 +38,15 @@ class SocketServer(com_base.BaseServer):
 
 
 class SocketClient(com_base.BaseClient):
-    def __init__(self) -> None:
+    def __init__(self, app: any) -> None:
         super().__init__()
+        self.app = app
+        self.sock = None
+        self.running = True
+
+    def destroy(self) -> None:
+        self.running = False
+        if self.sock:
+            self.sock.close()
+            self.sock = None
+        self.app = None
