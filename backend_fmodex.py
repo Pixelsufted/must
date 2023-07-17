e5ccb7d67d7d9567279180da09b714997e8cc0bb
Pixelsuft <yuskivl07@mail.ru>
2023-07-17 08:16:34 +0700

    prettify a little

--- backend_fmodex.py
+++ backend_fmodex.py
@@ -96,26 +96,39 @@ class FmodExWrapper(backend_base.BaseWrapper):
         self.FMOD_ERR_TOO_MANY_SAMPLES = 81
         self.error_map = {
             self.FMOD_OK: "No errors.",
-            self.FMOD_ERR_BAD_COMMAND: "Tried to call a function on a data type that does not allow this type of functionality (ie calling Sound::lock on a streaming sound).",
+            self.FMOD_ERR_BAD_COMMAND: "Tried to call a function on a data type that does not allow this type of"
+                                       " functionality (ie calling Sound::lock on a streaming sound).",
             self.FMOD_ERR_CHANNEL_ALLOC: "Error trying to allocate a channel.",
             self.FMOD_ERR_CHANNEL_STOLEN: "The specified channel has been reused to play another sound.",
             self.FMOD_ERR_DMA: "DMA Failure.  See debug output for more information.",
-            self.FMOD_ERR_DSP_CONNECTION: "DSP connection error.  Connection possibly caused a cyclic dependency or connected dsps with incompatible buffer counts.",
-            self.FMOD_ERR_DSP_DONT_PROCESS: "DSP return code from a DSP process query callback.  Tells mixer not to call the process callback and therefore not consume CPU.  Use this to optimize the DSP graph.",
-            self.FMOD_ERR_DSP_FORMAT: "DSP Format error.  A DSP unit may have attempted to connect to this network with the wrong format, or a matrix may have been set with the wrong size if the target unit has a specified channel map.",
-            self.FMOD_ERR_DSP_INUSE: "DSP is already in the mixer's DSP network. It must be removed before being reinserted or released.",
+            self.FMOD_ERR_DSP_CONNECTION: "DSP connection error.  Connection possibly caused a cyclic dependency or"
+                                          " connected dsps with incompatible buffer counts.",
+            self.FMOD_ERR_DSP_DONT_PROCESS: "DSP return code from a DSP process query callback.  Tells mixer not to"
+                                            " call the process callback and therefore not consume CPU.  Use this to"
+                                            " optimize the DSP graph.",
+            self.FMOD_ERR_DSP_FORMAT: "DSP Format error.  A DSP unit may have attempted to connect to this network"
+                                      " with the wrong format, or a matrix may have been set with the wrong size"
+                                      " if the target unit has a specified channel map.",
+            self.FMOD_ERR_DSP_INUSE: "DSP is already in the mixer's DSP network. It must be removed before being"
+                                     " reinserted or released.",
             self.FMOD_ERR_DSP_NOTFOUND: "DSP connection error.  Couldn't find the DSP unit specified.",
-            self.FMOD_ERR_DSP_RESERVED: "DSP operation error.  Cannot perform operation on this DSP as it is reserved by the system.",
-            self.FMOD_ERR_DSP_SILENCE: "DSP return code from a DSP process query callback.  Tells mixer silence would be produced from read, so go idle and not consume CPU.  Use this to optimize the DSP graph.",
+            self.FMOD_ERR_DSP_RESERVED: "DSP operation error.  Cannot perform operation on this DSP as it is reserved"
+                                        " by the system.",
+            self.FMOD_ERR_DSP_SILENCE: "DSP return code from a DSP process query callback.  Tells mixer silence would"
+                                       " be produced from read, so go idle and not consume CPU.  Use this to optimize"
+                                       " the DSP graph.",
             self.FMOD_ERR_DSP_TYPE: "DSP operation cannot be performed on a DSP of this type.",
             self.FMOD_ERR_FILE_BAD: "Error loading file.",
-            self.FMOD_ERR_FILE_COULD_NOT_SEEK: "Couldn't perform seek operation.  This is a limitation of the medium (ie netstreams) or the file format.",
+            self.FMOD_ERR_FILE_COULD_NOT_SEEK: "Couldn't perform seek operation.  This is a limitation of the medium"
+                                               " (ie netstreams) or the file format.",
             self.FMOD_ERR_FILE_DISK_EJECTED: "Media was ejected while reading.",
-            self.FMOD_ERR_FILE_EOF: "End of file unexpectedly reached while trying to read essential data (truncated?).",
+            self.FMOD_ERR_FILE_EOF: "End of file unexpectedly reached while trying to read essential data"
+                                    " (truncated?).",
             self.FMOD_ERR_FILE_END_OF_DATA: "End of current chunk reached while trying to read data.",
             self.FMOD_ERR_FILE_NOTFOUND: "File not found.",
             self.FMOD_ERR_FORMAT: "Unsupported file or audio format.",
-            self.FMOD_ERR_HEADER_MISMATCH: "There is a version mismatch between the FMOD header and either the FMOD Studio library or the FMOD Low Level library.",
+            self.FMOD_ERR_HEADER_MISMATCH: "There is a version mismatch between the FMOD header and either the FMOD"
+                                           " Studio library or the FMOD Low Level library.",
             self.FMOD_ERR_HTTP: "A HTTP error occurred. This is a catch-all for HTTP errors not listed elsewhere.",
             self.FMOD_ERR_HTTP_ACCESS: "The specified resource requires authentication or is forbidden.",
             self.FMOD_ERR_HTTP_PROXY_AUTH: "Proxy authentication is required to access the specified resource.",
@@ -128,46 +141,70 @@ class FmodExWrapper(backend_base.BaseWrapper):
             self.FMOD_ERR_INVALID_HANDLE: "An invalid object handle was used.",
             self.FMOD_ERR_INVALID_PARAM: "An invalid parameter was passed to this function.",
             self.FMOD_ERR_INVALID_POSITION: "An invalid seek position was passed to this function.",
-            self.FMOD_ERR_INVALID_SPEAKER: "An invalid speaker was passed to this function based on the current speaker mode.",
+            self.FMOD_ERR_INVALID_SPEAKER: "An invalid speaker was passed to this function based on the current"
+                                           " speaker mode.",
             self.FMOD_ERR_INVALID_SYNC_POINT: "The syncpoint did not come from this sound handle.",
             self.FMOD_ERR_INVALID_THREAD: "Tried to call a function on a thread that is not supported.",
             self.FMOD_ERR_INVALID_VECTOR: "The vectors passed in are not unit length, or perpendicular.",
             self.FMOD_ERR_MAX_AUDIBLE: "Reached maximum audible playback count for this sound's soundgroup.",
             self.FMOD_ERR_MEMORY: "Not enough memory or resources.",
-            self.FMOD_ERR_MEMORY_CANT_POINT: "Can't use FMOD_OPENMEMORY_POINT on non PCM source data, or non mp3/xma/adpcm data if FMOD_CREATECOMPRESSEDSAMPLE was used.",
+            self.FMOD_ERR_MEMORY_CANT_POINT: "Can't use FMOD_OPENMEMORY_POINT on non PCM source data, or non"
+                                             " mp3/xma/adpcm data if FMOD_CREATECOMPRESSEDSAMPLE was used.",
             self.FMOD_ERR_NEEDS3D: "Tried to call a command on a 2d sound when the command was meant for 3d sound.",
             self.FMOD_ERR_NEED_SHARD_WARE: "Tried to use a feature that requires hardware support.",
             self.FMOD_ERR_NET_CONNECT: "Couldn't connect to the specified host.",
-            self.FMOD_ERR_NET_SOCKET_ERROR: "A socket error occurred.  This is a catch-all for socket-related errors not listed elsewhere.",
+            self.FMOD_ERR_NET_SOCKET_ERROR: "A socket error occurred.  This is a catch-all for socket-related errors"
+                                            " not listed elsewhere.",
             self.FMOD_ERR_NET_URL: "The specified URL couldn't be resolved.",
             self.FMOD_ERR_NET_WOULD_BLOCK: "Operation on a non-blocking socket could not complete immediately.",
-            self.FMOD_ERR_NOT_READY: "Operation could not be performed because specified sound/DSP connection is not ready.",
-            self.FMOD_ERR_OUTPUT_ALLOCATED: "Error initializing output device, but more specifically, the output device is already in use and cannot be reused.",
+            self.FMOD_ERR_NOT_READY: "Operation could not be performed because specified sound/DSP connection is not"
+                                     " ready.",
+            self.FMOD_ERR_OUTPUT_ALLOCATED: "Error initializing output device, but more specifically, the output device"
+                                            " is already in use and cannot be reused.",
             self.FMOD_ERR_OUTPUT_CREATE_BUFFER: "Error creating hardware sound buffer.",
-            self.FMOD_ERR_OUTPUT_DRIVER_CALL: "A call to a standard soundcard driver failed, which could possibly mean a bug in the driver or resources were missing or exhausted.",
+            self.FMOD_ERR_OUTPUT_DRIVER_CALL: "A call to a standard soundcard driver failed, which could possibly mean"
+                                              " a bug in the driver or resources were missing or exhausted.",
             self.FMOD_ERR_OUTPUT_FORMAT: "Soundcard does not support the specified format.",
             self.FMOD_ERR_OUTPUT_INIT: "Error initializing output device.",
-            self.FMOD_ERR_OUTPUT_NO_DRIVERS: "The output device has no drivers installed.  If pre-init, FMOD_OUTPUT_NOSOUND is selected as the output mode.  If post-init, the function just fails.",
+            self.FMOD_ERR_OUTPUT_NO_DRIVERS: "The output device has no drivers installed.  If pre-init,"
+                                             " FMOD_OUTPUT_NOSOUND is selected as the output mode.  If post-init,"
+                                             " the function just fails.",
             self.FMOD_ERR_PLUGIN: "An unspecified error has been returned from a plugin.",
             self.FMOD_ERR_PLUGIN_MISSING: "A requested output, dsp unit type or codec was not available.",
-            self.FMOD_ERR_PLUGIN_RESOURCE: "A resource that the plugin requires cannot be allocated or found. (ie the DLS file for MIDI playback)",
+            self.FMOD_ERR_PLUGIN_RESOURCE: "A resource that the plugin requires cannot be allocated or found."
+                                           " (ie the DLS file for MIDI playback)",
             self.FMOD_ERR_PLUGIN_VERSION: "A plugin was built with an unsupported SDK version.",
             self.FMOD_ERR_RECORD: "An error occurred trying to initialize the recording device.",
-            self.FMOD_ERR_REVERB_CHANNEL_GROUP: "Reverb properties cannot be set on this channel because a parent channelgroup owns the reverb connection.",
-            self.FMOD_ERR_REVERB_INSTANCE: "Specified instance in FMOD_REVERB_PROPERTIES couldn't be set. Most likely because it is an invalid instance number or the reverb doesn't exist.",
-            self.FMOD_ERR_SUB_SOUNDS: "The error occurred because the sound referenced contains subsounds when it shouldn't have, or it doesn't contain subsounds when it should have.  The operation may also not be able to be performed on a parent sound.",
-            self.FMOD_ERR_SUB_SOUND_ALLOCATED: "This subsound is already being used by another sound, you cannot have more than one parent to a sound.  Null out the other parent's entry first.",
-            self.FMOD_ERR_SUB_SOUND_CANT_MOVE: "Shared subsounds cannot be replaced or moved from their parent stream, such as when the parent stream is an FSB file.",
+            self.FMOD_ERR_REVERB_CHANNEL_GROUP: "Reverb properties cannot be set on this channel because a parent"
+                                                " channelgroup owns the reverb connection.",
+            self.FMOD_ERR_REVERB_INSTANCE: "Specified instance in FMOD_REVERB_PROPERTIES couldn't be set. Most likely"
+                                           " because it is an invalid instance number or the reverb doesn't exist.",
+            self.FMOD_ERR_SUB_SOUNDS: "The error occurred because the sound referenced contains subsounds when it"
+                                      " shouldn't have, or it doesn't contain subsounds when it should have."
+                                      "  The operation may also not be able to be performed on a parent sound.",
+            self.FMOD_ERR_SUB_SOUND_ALLOCATED: "This subsound is already being used by another sound, you cannot have"
+                                               " more than one parent to a sound.  Null out the other parent's entry"
+                                               " first.",
+            self.FMOD_ERR_SUB_SOUND_CANT_MOVE: "Shared subsounds cannot be replaced or moved from their parent stream,"
+                                               " such as when the parent stream is an FSB file.",
             self.FMOD_ERR_TAG_NOT_FOUND: "The specified tag could not be found or there are no tags.",
-            self.FMOD_ERR_TOO_MANY_CHANNELS: "The sound created exceeds the allowable input channel count.  This can be increased using the 'maxinputchannels' parameter in System::setSoftwareFormat.",
-            self.FMOD_ERR_TRUNCATED: "The retrieved string is too long to fit in the supplied buffer and has been truncated.",
-            self.FMOD_ERR_UNIMPLEMENTED: "Something in FMOD hasn't been implemented when it should be! contact support!",
-            self.FMOD_ERR_UNINITIALIZED: "This command failed because System::init or System::setDriver was not called.",
-            self.FMOD_ERR_UNSUPPORTED: "A command issued was not supported by this object.  Possibly a plugin without certain callbacks specified.",
+            self.FMOD_ERR_TOO_MANY_CHANNELS: "The sound created exceeds the allowable input channel count.  This can be"
+                                             " increased using the 'maxinputchannels' parameter in"
+                                             " System::setSoftwareFormat.",
+            self.FMOD_ERR_TRUNCATED: "The retrieved string is too long to fit in the supplied buffer and has been"
+                                     " truncated.",
+            self.FMOD_ERR_UNIMPLEMENTED: "Something in FMOD hasn't been implemented when it should be!"
+                                         " contact support!",
+            self.FMOD_ERR_UNINITIALIZED: "This command failed because System::init or System::setDriver"
+                                         " was not called.",
+            self.FMOD_ERR_UNSUPPORTED: "A command issued was not supported by this object.  Possibly a plugin without"
+                                       " certain callbacks specified.",
             self.FMOD_ERR_VERSION: "The version number of this file format is not supported.",
             self.FMOD_ERR_EVENT_ALREADY_LOADED: "The specified bank has already been loaded.",
-            self.FMOD_ERR_EVENT_LIVE_UPDATE_BUSY: "The live update connection failed due to the game already being connected.",
-            self.FMOD_ERR_EVENT_LIVE_UPDATE_MISMATCH: "The live update connection failed due to the game data being out of sync with the tool.",
+            self.FMOD_ERR_EVENT_LIVE_UPDATE_BUSY: "The live update connection failed due to the game already being"
+                                                  " connected.",
+            self.FMOD_ERR_EVENT_LIVE_UPDATE_MISMATCH: "The live update connection failed due to the game data being"
+                                                      " out of sync with the tool.",
             self.FMOD_ERR_EVENT_LIVE_UPDATE_TIMEOUT: "The live update connection timed out.",
             self.FMOD_ERR_EVENT_NOTFOUND: "The requested event, parameter, bus or vca could not be found.",
             self.FMOD_ERR_STUDIO_UNINITIALIZED: "The Studio::System object is not yet initialized.",
